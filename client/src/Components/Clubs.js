8522230c06be40281f63e9bd304ee70532cd0b43
collins <kipkorirc583@gmail.com>
2023-11-09 08:49:58 +0300

    displaying of Clubs and creation of clubs

--- client/src/Components/Clubs.js
+++ client/src/Components/Clubs.js
@@ -1,368 +1,186 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Clubs= () => {
-  // State variables
-  const [clubsData, setClubsData] = useState([]);
-  const [postsData, setPostsData] = useState([]);
-  const [expandedClub, setExpandedClub] = useState(null);
+const Clubs = () => {
+  const [clubs, setClubs] = useState([]);
   const [searchText, setSearchText] = useState('');
-  const [showCreateClubForm, setShowCreateClubForm] = useState(false);
-  const [newClubData, setNewClubData] = useState({
-    clubName: '',
-    clubGenre: '',
-    description: '',
-    selectedMembers: [],
-  });
-  const [allMembers, setAllMembers] = useState([]);
+  const [showCreateForm, setShowCreateForm] = useState(false);
+  const [newClub, setNewClub] = useState({ name: '', genre: '', description: '' });
+  const [notification, setNotification] = useState('');
+  const [selectedClubId, setSelectedClubId] = useState(null);
+  const [posts, setPosts] = useState([]);
 
-  // useEffect for fetching data from APIs
   useEffect(() => {
-    // Fetch clubs data
-    axios
-      .get('https://flickfeeds-602d4f3e68d7.herokuapp.com/clubs')
-      .then((response) => {
-        setClubsData(response.data.clubs);
+    axios.get('https://flickfeeds-602d4f3e68d7.herokuapp.com/clubs')
+      .then(response => {
+        setClubs(response.data.clubs);
       })
-      .catch((error) => {
-        console.error('Error fetching clubs data:', error);
-      });
-
-    // Fetch posts data
-    axios
-      .get('https://flickfeeds-602d4f3e68d7.herokuapp.com/posts')
-      .then((response) => {
-        setPostsData(response.data.posts);
-      })
-      .catch((error) => {
-        console.error('Error fetching posts data:', error);
-      });
-
-    // Fetch all members data from your API or source
-    // Replace 'https://your-members-api-url' with the actual API endpoint
-    axios
-      .get('https://your-members-api-url')
-      .then((response) => {
-        setAllMembers(response.data.members);
-      })
-      .catch((error) => {
-        console.error('Error fetching members data:', error);
-      });
+      .catch(error => console.error('Error fetching clubs:', error));
   }, []);
 
-  // Function to generate club descriptions
-  const generateClubDescription = (club) => {
-    return `Welcome to the ${club.Name} club! This is a community of ${club.Genre} enthusiasts. ${club.Description}`;
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewClub(prevState => ({ ...prevState, [name]: value }));
   };
 
-  // Function to toggle the display of the "Posts" section
-  const handleTogglePosts = (club) => {
-    if (expandedClub === club.ClubID) {
-      setExpandedClub(null); // Collapse the "Posts" section if it's already open
-    } else {
-      setExpandedClub(club.ClubID); // Expand the "Posts" section for the clicked club
-    }
-  };
-
-  // Shuffle the postsData array
-  const shuffledPosts = [...postsData].sort(() => Math.random() - 0.5);
-
-  // Function to handle clicking "Create Club" button
-  const handleCreateClubClick = () => {
-    setShowCreateClubForm(!showCreateClubForm); // Toggle the form display
-  };
-
-  // Function to handle selecting a member in the create club form
-  const handleMemberSelection = (event) => {
-    const selectedMemberId = event.target.value;
-    const selectedMember = allMembers.find(
-      (member) => member.id === selectedMemberId
-    );
-
-    if (selectedMember) {
-      // Add the selected member to the list of selected members
-      setNewClubData({
-        ...newClubData,
-        selectedMembers: [...newClubData.selectedMembers, selectedMember],
-      });
-    }
-  };
-
-  // Function to handle joining a club
-  const handleJoinClubClick = (club) => {
-    // You can add code here to handle joining the club
-    console.log('Joined club:', club.Name);
-  };
-
-  // Filter clubs based on the search text
-  const filteredClubs = clubsData.filter((club) =>
-    club.Name.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  // Function to handle the "Search" button click
-  const handleSearchClick = () => {
-    // Add your search logic here
-    console.log('Search clicked with text:', searchText);
-  };
-
-  // Function to handle form submission when creating a club
-  const handleCreateClubFormSubmit = (event) => {
+  const handleCreateClub = (event) => {
     event.preventDefault();
-
-    // You can add code here to handle the form submission and make a POST request to create a new club
-    // For now, we'll just log the form data to the console
-    console.log('Form submitted:', newClubData);
-
-    // After handling the form submission, you can reset the form and close it
-    setShowCreateClubForm(false);
+    axios.post('https://flickfeeds-602d4f3e68d7.herokuapp.com/create_club', newClub)
+      .then(response => {
+        setClubs([...clubs, response.data]);
+        setNotification('Club created successfully!');
+        setShowCreateForm(false);
+        setNewClub({ name: '', genre: '', description: '' });
+      })
+      .catch(error => console.error('Error creating club:', error));
   };
 
-  // CSS for the green buttons
-  const greenButtonStyle = {
-    backgroundColor: 'red',
-    color: 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
+  const handleJoinClub = (clubId) => {
+    axios.post(`https://flickfeeds-602d4f3e68d7.herokuapp.com/join_club/${clubId}`)
+      .then(response => {
+        setNotification('Joined club successfully!');
+      })
+      .catch(error => console.error('Error joining club:', error));
   };
 
-  // CSS for the Create Club form
-  const createClubFormStyle = {
-    backgroundColor: 'white',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)',
+  const handleViewPosts = (clubId) => {
+    if (selectedClubId === clubId) {
+      setSelectedClubId(null);
+      setPosts([]);
+    } else {
+      setSelectedClubId(clubId);
+      axios.get(`https://flickfeeds-602d4f3e68d7.herokuapp.com/posts/${clubId}`)
+        .then(response => {
+          setPosts(response.data.posts);
+        })
+        .catch(error => console.error('Error fetching posts:', error));
+    }
   };
 
-  // CSS for text fields and labels
-  const formFieldStyle = {
-    marginBottom: '15px',
+  const filteredClubs = clubs.filter(club => club.name.toLowerCase().includes(searchText.toLowerCase()));
+
+  const styles = {
+    container: {
+      backgroundColor: '#fff',
+      color: '#333',
+      padding: '20px',
+      borderRadius: '8px',
+      boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+      marginBottom: '10px',
+    },
+    header: {
+      backgroundColor: 'red',
+      color: '#fff',
+      padding: '10px',
+      borderRadius: '4px',
+    },
+    button: {
+      backgroundColor: 'red',
+      color: '#fff',
+      border: 'none',
+      padding: '10px 15px',
+      margin: '5px',
+      borderRadius: '4px',
+      cursor: 'pointer',
+    },
+    input: {
+      padding: '10px',
+      margin: '5px 0',
+      borderRadius: '4px',
+      border: '1px solid #ddd',
+      width: '100%',
+    },
+    notification: {
+      position: 'absolute',
+      top: '10px',
+      right: '10px',
+      backgroundColor: 'black',
+      color: 'white',
+      padding: '10px',
+      borderRadius: '5px',
+      zIndex: 1000,
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      marginBottom: '10px',
+    },
+    postImage: {
+      width: '80px',
+      height: '80px',
+      borderRadius: '4px',
+      marginRight: '10px',
+    }
   };
 
-  const labelStyle = {
-    display: 'block',
-    marginBottom: '5px',
-    fontWeight: 'bold',
-  };
-
-  const inputStyle = {
-    width: '100%',
-    padding: '10px',
-    borderRadius: '5px',
-    border: '1px solid #ccc',
-  };
-
-  // JSX for rendering the component
   return (
-    <div className="club-card">
-      <div className="search-and-create">
-        <input
-          type="text"
-          placeholder="Search Club"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <button onClick={handleSearchClick} style={greenButtonStyle}>
-          Search
-        </button>
-        <button onClick={handleCreateClubClick} style={greenButtonStyle}>
-          {showCreateClubForm ? 'Cancel' : 'Create Club'}
-        </button>
-      </div>
-
-      {showCreateClubForm && (
-        <div className="create-club-form" style={createClubFormStyle}>
-          <h3>Create Club</h3>
-          <form onSubmit={handleCreateClubFormSubmit}>
-            <div style={formFieldStyle}>
-              <label htmlFor="clubName" style={labelStyle}>
-                Club Name:
-              </label>
-              <input
-                type="text"
-                id="clubName"
-                name="clubName"
-                required
-                style={inputStyle}
-                onChange={(e) =>
-                  setNewClubData({ ...newClubData, clubName: e.target.value })
-                }
-              />
-            </div>
-            <div style={formFieldStyle}>
-              <label htmlFor="clubGenre" style={labelStyle}>
-                Club Genre:
-              </label>
-              <input
-                type="text"
-                id="clubGenre"
-                name="clubGenre"
-                required
-                style={inputStyle}
-                onChange={(e) =>
-                  setNewClubData({ ...newClubData, clubGenre: e.target.value })
-                }
-              />
-            </div>
-            <div style={formFieldStyle}>
-              <label htmlFor="description" style={labelStyle}>
-                Description:
-              </label>
-              <textarea
-                id="description"
-                name="description"
-                style={{
-                  ...inputStyle,
-                  height: '100px', // Adjust the height as needed
-                }}
-                onChange={(e) =>
-                  setNewClubData({
-                    ...newClubData,
-                    description: e.target.value,
-                  })
-                }
-              />
-            </div>
-            <div style={formFieldStyle}>
-              <label htmlFor="members" style={labelStyle}>
-                Members:
-              </label>
-              <select
-                id="members"
-                name="members"
-                onChange={handleMemberSelection}
-                style={inputStyle}
-              >
-                <option value="">Select  members</option>
-                {allMembers.map((member) => (
-                  <option key={member.id} value={member.id}>
-                    {member.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div style={formFieldStyle}>
-              <button type="submit" style={greenButtonStyle}>
-                Create
-              </button>
-            </div>
-          </form>
+    <div style={{ backgroundColor: 'black', padding: '20px' }}>
+      {notification && <div style={styles.notification}>{notification}</div>}
+      <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+        <div style={{ ...styles.container, backgroundColor: 'white' }}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search clubs..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            style={styles.input}
+          />
+          <button onClick={() => setShowCreateForm(!showCreateForm)} style={styles.button}>
+            {showCreateForm ? 'Cancel' : 'Create Club'}
+          </button>
         </div>
-      )}
 
-      <div className="club-card-container">
-        {filteredClubs.map((club, index) => {
-          // Filter posts that belong to the current club
-          const clubPosts = shuffledPosts.filter(
-            (post) => post.MovieID === club.ClubID
-          );
+        {showCreateForm && (
+          <div style={{ ...styles.container, backgroundColor: 'white' }}>
+            <form onSubmit={handleCreateClub} style={styles.form}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Club Name"
+                value={newClub.name}
+                onChange={handleInputChange}
+                style={styles.input}
+              />
+              <input
+                type="text"
+                name="genre"
+                placeholder="Club Genre"
+                value={newClub.genre}
+                onChange={handleInputChange}
+                style={styles.input}
+              />
+              <textarea
+                name="description"
+                placeholder="Description"
+                value={newClub.description}
+                onChange={handleInputChange}
+                style={{ ...styles.input, height: '100px' }}
+              />
+              <button type="submit" style={styles.button}>Create Club</button>
+            </form>
+          </div>
+        )}
 
-          // Randomly select 3 to 6 posts for the current club
-          const selectedClubPosts = clubPosts.slice(
-            0,
-            Math.floor(Math.random() * (6 - 3 + 1)) + 3
-          );
+        {filteredClubs.map(club => (
+          <div key={club.id} style={{ ...styles.container, backgroundColor: 'white' }}>
+            <div style={styles.header}>{club.name}</div>
+            <p>Genre: {club.genre}</p>
+            <p>{club.description}</p>
+            <button onClick={() => handleViewPosts(club.id)} style={styles.button}>View Posts</button>
+            <button onClick={() => handleJoinClub(club.id)} style={styles.button}>Join Club</button>
+          </div>
+        ))}
 
-          return (
-            <div
-              key={index}
-              className={`club-card-item ${
-                expandedClub === club.ClubID ? 'expanded-club' : ''
-              }`}
-              style={{
-                border: '1px solid #ccc',
-                borderRadius: '8px',
-                margin: '20px',
-                padding: '20px',
-                width: '300px',
-                display: 'inline-block',
-                boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)',
-                position: 'relative', // Ensure that the expanded club stays on top
-              }}
-            >
-              <h2
-                style={{
-                  fontSize: '20px',
-                  fontWeight: 'bold',
-                  textAlign: 'center',
-                }}
-              >
-                {club.Name}
-              </h2>
-              <p
-                style={{
-                  fontSize: '16px',
-                  textAlign: 'center',
-                }}
-              >
-                Genre: {club.Genre}
-              </p>
-              <p
-                style={{
-                  fontSize: '14px',
-                }}
-              >
-                {generateClubDescription(club)}
-              </p>
-              <h3>Members</h3>
-              <ul
-                className="members-list"
-                style={{
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  justifyContent: 'center',
-                }}
-              >
-                {newClubData.selectedMembers.map((member) => (
-                  <li key={member.id}>{member.name}</li>
-                ))}
-              </ul>
-              {/* Make the "Posts" text clickable */}
-              <button
-                style={{
-                  backgroundColor: 'red',
-                  color: 'white',
-                  width: '100%',
-                  marginTop: '10px',
-                }}
-                onClick={() => handleTogglePosts(club)}
-              >
-                View Posts
-              </button>
-              {expandedClub === club.ClubID && (
-                <div className="post-card" style={{ marginTop: '10px' }}>
-                  {selectedClubPosts.map((post, postIndex) => (
-                    <div key={postIndex} className="post-item">
-                      <img
-                        src={post.ImagePath}
-                        alt="Post"
-                        style={{
-                          width: '80px', // Set the image size to 50px by 50px
-                          height: '80px',
-                        }}
-                      />
-                      <p>Rating: {post.Rating}</p>
-                      <p>Review: {post.Review}</p>
-                      {/* Display comments here */}
-                    </div>
-                  ))}
-                </div>
-              )}
-              <button
-                style={{
-                  backgroundColor: 'red',
-                  color: 'white',
-                  width: '100%',
-                  marginTop: '10px',
-                }}
-                onClick={() => handleJoinClubClick(club)}
-              >
-                Join Club
-              </button>
+        {selectedClubId && posts.map(post => (
+          <div key={post.id} style={{ ...styles.container, display: 'flex', alignItems: 'center', backgroundColor: 'white' }}>
+            <img src={post.imagePath} alt="Post" style={styles.postImage} />
+            <div>
+              <p>Rating: {post.rating}</p>
+              <p>{post.review}</p>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
