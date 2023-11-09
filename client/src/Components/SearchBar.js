d6e5934e2426812a8b51724245b9af596ff1d215
collins <kipkorirc583@gmail.com>
2023-11-09 21:09:15 +0300

    correction on search

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -4,12 +4,14 @@ import theme from './theme';
 
 const SearchBar = ({ onSearch }) => {
   return (
-    <input
-      type="text"
-      placeholder="Search Clubs..."
-      onChange={(e) => onSearch(e.target.value)}
-      style={theme.searchBar}
-    />
+    <div style={{ textAlign: 'center' }}>
+      <input
+        type="text"
+        placeholder="Search Clubs..."
+        onChange={(e) => onSearch(e.target.value)}
+        style={theme.searchBar}
+      />
+    </div>
   );
 };
 
