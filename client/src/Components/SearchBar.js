0b15880ac23c2079be16511dd6e16018c535f66f
victor odambatafwa <omondivictoras@gmail.com>
2023-11-10 12:22:08 +0300

    Made changes in routing

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,16 +1,14 @@
-// SearchBar.js
 import React from 'react';
-import theme from './theme';
 
 const SearchBar = ({ onSearch }) => {
+  const searchBarStyle = {
+    margin: '10px 0',
+  };
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <input
-        type="text"
-        placeholder="Search Clubs..."
-        onChange={(e) => onSearch(e.target.value)}
-        style={theme.searchBar}
-      />
+    <div style={searchBarStyle}>
+      <input type="text" placeholder="Search clubs..." onChange={(e) => onSearch(e.target.value)} />
+      <button onClick={() => onSearch(document.querySelector('input').value)}>Search</button>
     </div>
   );
 };
